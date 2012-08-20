241aec6a0682d5ef047961d378dc8dcf85acca09
Zsolt Fabok <me@zsoltfabok.com>
2012-08-20 21:23:36 +0300

    delete the temporary folder tomcat.8080 during stop and add a method in order to know when the service is running or not

--- src/test/java/com/zsoltfabok/blog/EmbeddedTomcat.java
+++ src/test/java/com/zsoltfabok/blog/EmbeddedTomcat.java
@@ -8,10 +8,11 @@ import org.apache.catalina.startup.Tomcat;
 import org.apache.commons.io.FileUtils;
 
 public class EmbeddedTomcat {
-  private Tomcat tomcat = new Tomcat();
+  private Tomcat tomcat;
 
   public void start() {
     try {
+      tomcat = new Tomcat();
       // If I don't want to copy files around then the base directory must be '.'
       String baseDir = ".";
       tomcat.setPort(8090);
@@ -31,6 +32,7 @@ public class EmbeddedTomcat {
       tomcat.destroy();
       // Tomcat creates a work folder where the temporary files are stored
       FileUtils.deleteDirectory(new File("work"));
+      FileUtils.deleteDirectory(new File("tomcat.8080"));
     } catch (LifecycleException e) {
       throw new RuntimeException(e);
     } catch (IOException e) {
@@ -45,5 +47,9 @@ public class EmbeddedTomcat {
   public String getApplicationUrl(String appName) {
     return String.format("http://%s:%d/%s", tomcat.getHost().getName(),
         tomcat.getConnector().getLocalPort(), appName);
+  }
+
+  public boolean isRunning() {
+      return tomcat != null;
   }
 }
